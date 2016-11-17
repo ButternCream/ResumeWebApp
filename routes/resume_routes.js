91417045fad072c2097d1b56aef80a4246894842
Scott Mitchell <mitchsco@sonoma.edu>
2016-11-16 18:47:39 -0800

    Added resume add / delete functions

--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -35,4 +35,58 @@ router.get('/', function(req, res){
     }
 });
 
+// Return the add a new school form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    account_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('resume/resumeAdd', {'resume': result});
+        }
+    });
+});
+
+// View the resume for the given id
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.resume_name == null) {
+        res.send('Resume Name must be provided.');
+    }
+    else if(req.query.account_id == null) {
+        res.send('An Account must be selected');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        account_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
+            }
+        });
+    }
+});
+
+// Delete a resume for the given resume-id
+router.get('/delete', function(req, res){
+    if(req.query.resume_id == null) {
+        res.send('resume_id is null');
+    }
+    else {
+        account_dal.delete(req.query.resume_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
+            }
+        });
+    }
+});
+
 module.exports = router;
